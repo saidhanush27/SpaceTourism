0bd612c47715961d17d820d96ac136e0874643bc
saidhanush27 <sai2003vr@gmail.com>
2025-01-24 14:24:21 +0530

    Initial commit

---
+++ tabs.js
@@ -0,0 +1,80 @@
+
+
+const tabList = document.querySelector('[role="tablist"]');
+const tabs = tabList.querySelectorAll('[role="tab"]');
+
+tabList.addEventListener('keydown', changeTabFocus);
+
+tabs.forEach((tab) => {
+    tab.addEventListener('click', changeTabPanel);
+});
+
+
+let tabFocus = 0;
+function changeTabFocus(e) {
+    const keydownLeft = 37;
+    const keydownRight = 39;
+    
+    if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
+        tabs[tabFocus].setAttribute("tabindex", -1);
+        
+        if (e.keyCode === keydownRight) {
+            tabFocus++;
+            if (tabFocus >= tabs.length) {
+                tabFocus = 0;
+            }
+        } else if (e.keyCode === keydownLeft) {
+            tabFocus--;
+            if (tabFocus < 0) {
+                tabFocus = tabs.length - 1;
+            }
+        }
+        
+        tabs[tabFocus].setAttribute("tabindex", 0);
+        tabs[tabFocus].focus();
+    }
+}
+
+
+function changeTabPanel(e) {
+    const targetTab = e.target;
+    const targetPanel = targetTab.getAttribute("aria-controls");
+    const targetImage = targetTab.getAttribute("data-image");
+    const targetVideo = targetTab.getAttribute("data-image");
+    // const targetCrousel = targetTab.getAttribute("data-slide")
+
+
+    
+    const tabContainer = targetTab.parentNode;
+    const mainContainer = tabContainer.parentNode;
+    
+    tabContainer
+        .querySelector('[aria-selected="true"]')
+        .setAttribute("aria-selected", false);
+        
+    targetTab.setAttribute("aria-selected", true);
+    
+    hideContent(mainContainer, '[role="tabpanel"]');
+    showContent(mainContainer, [`#${targetPanel}`]);
+    
+    hideContent(mainContainer, 'picture');
+    showContent(mainContainer, [`#${targetImage}`]);
+
+    hideContent(mainContainer, 'video');
+    showContent(mainContainer, [`#${targetVideo}`]);
+
+    // hideContent(mainContainer, 'div');
+    // showContent(mainContainer, [`#${targetCrousel}`]);
+
+
+}
+
+function hideContent(parent, content) {
+    parent
+        .querySelectorAll(content)
+        .forEach((item) => item.setAttribute("hidden", true));
+}
+
+function showContent(parent, content) {
+     parent.querySelector(content).removeAttribute('hidden');
+}
